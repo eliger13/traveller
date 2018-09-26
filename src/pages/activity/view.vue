a32acaa85a35801e32b1a108494b64f9ecf7c59f
Eliger Roa <eliger13@gmail.com>
2018-09-26 02:50:38 +0000

    Modificar los metodos

--- src/pages/activity/view.vue
+++ src/pages/activity/view.vue
@@ -6,109 +6,112 @@
       <div class= "col-8">
         <q-list>
           <q-item>
-            <q-item-side icon="photo_album" />
+            <q-item-side icon="photo_album" color="primary" />
             <q-item-main>
               <q-item-tile label> Category </q-item-tile>
-              <q-item-tile sublabel>{{category}}</q-item-tile>
+              <q-item-tile sublabel>{{ getCategoryLabel() }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="terrain" />
+            <q-item-side icon="terrain" color="primary" />
             <q-item-main >
               <q-item-tile label> Activity </q-item-tile>
-              <q-item-tile sublabel>{{activity}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.activity }}</q-item-tile>
             </q-item-main>
           </q-item>
 
+          <q-item-separator inset />
           <q-item>
-            <q-item-side icon="assignment_ind" />
+            <q-item-side icon="assignment_ind" color="primary" />
             <q-item-main >
               <q-item-tile label> Host </q-item-tile>
-              <q-item-tile sublabel>{{host}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.host }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="place" />
+            <q-item-side icon="place" color="primary" />
             <q-item-main >
               <q-item-tile label>Address </q-item-tile>
-              <q-item-tile sublabel>{{address}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.address }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="outlined_flag" />
+            <q-item-side icon="outlined_flag" color="primary" />
             <q-item-main >
               <q-item-tile label> Country </q-item-tile>
-              <q-item-tile sublabel>{{country}}</q-item-tile>
+              <q-item-tile sublabel>{{ getCountry() }}</q-item-tile>
             </q-item-main>
           </q-item>
 
+          <q-item-separator inset />
           <q-item>
-            <q-item-side icon="language" />
+            <q-item-side icon="language" color="primary" />
             <q-item-main >
               <q-item-tile label> Language </q-item-tile>
-              <q-item-tile sublabel>{{language}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.language }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="description" />
+            <q-item-side icon="description" color="primary" />
             <q-item-main >
               <q-item-tile label> Description </q-item-tile>
-              <q-item-tile sublabel>{{description}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.description }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="receipt" />
+            <q-item-side icon="receipt" color="primary" />
             <q-item-main >
               <q-item-tile label> Requirements </q-item-tile>
-              <q-item-tile sublabel>{{requirements}}</q-item-tile>
+              <q-item-tile sublabel>{{ form.requirements }}</q-item-tile>
             </q-item-main>
           </q-item>
 
+          <q-item-separator inset />
           <q-item>
-            <q-item-side icon="monetization_on" />
+            <q-item-side icon="monetization_on" color="primary" />
             <q-item-main >
               <q-item-tile label> Price </q-item-tile>
-              <q-item-tile sublabel>US{{price}}</q-item-tile>
+              <q-item-tile sublabel>US{{ form.price }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item-separator />
 
-          <q-list-header>Schedule </q-list-header>
+          <q-list-header> Schedule </q-list-header>
           <q-item>
-            <q-item-side icon="date_range" />
+            <q-item-side icon="date_range" color="primary" />
             <q-item-main>
               <q-item-tile label> Days</q-item-tile>
-              <q-item-tile sublabel>{{daysOfWeek}}</q-item-tile>
+              <q-item-tile sublabel>{{ getDays() }}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="update" />
+            <q-item-side icon="update" color="primary" />
               <q-item-main>
               <q-item-tile label> Start </q-item-tile>
-              <q-item-tile sublabel>{{start}}</q-item-tile>
+              <q-item-tile sublabel>{{form.start}}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item>
-            <q-item-side icon="update"/>
+            <q-item-side icon="update" color="primary" />
             <q-item-main>
               <q-item-tile label> End</q-item-tile>
-              <q-item-tile sublabel>{{end}}</q-item-tile>
+              <q-item-tile sublabel>{{form.end}}</q-item-tile>
             </q-item-main>
           </q-item>
 
           <q-item-separator />
 
-          <q-list-header>Documentaries</q-list-header>
+          <q-list-header> Documentaries </q-list-header>
           <q-item>
-            <q-item-side image="" />
+            <q-item-side />
             <q-item-main label="Download Photo " />
             <q-item-side right icon="photo" />
           </q-item>
@@ -132,19 +135,21 @@ export default {
   data() {
     return {
       title: 'Activity Experiences',
-      category: 'Deportes extremos',
-      activity: 'activity',
-      host: 'host',
-      address: 'address',
-      country: 'conutry',
-      language: [],
-      description: 'descripton',
-      price: 0,
-      requirements: 'requirements',
-      daysOfWeek: [],
-      start: 0,
-      end: 0,
-      photos: '',
+      form: {
+        category: 'nature',
+        activity: 'Montañismo',
+        host: 'host',
+        address: 'address',
+        country: 'asia',
+        language: ['ing', 'esp'],
+        description: 'description',
+        price: 0,
+        requirements: 'requirements',
+        days: [1, 2, 3],
+        start: 0,
+        end: 0,
+        photos: '',
+      },
       categories: [
         {
           label: 'Naturaleza',
@@ -184,26 +189,26 @@ export default {
       listOfLanguages: [
         {
           label: 'Ingles',
-          value: 'Ing',
+          value: 'ing',
         },
         {
           label: 'Español',
-          value: 'Esp',
+          value: 'esp',
         },
         {
           label: 'Frances',
-          value: 'Fr',
+          value: 'fr',
         },
         {
           label: 'Portugues',
-          value: 'Portu',
+          value: 'portu',
         },
         {
           label: 'Mandarin',
-          value: 'Mand',
+          value: 'mand',
         },
       ],
-      listDaysOfWeek: [
+      DaysOfWeek: [
         {
           label: 'Lunes',
           value: 1,
@@ -238,6 +243,30 @@ export default {
   methods: {
     submit() {
       this.$router.push('/activity/edit/');
+    },
+    getCategoryLabel() {
+      for (let i = 0; i < this.categories.length; i += 1) {
+        if (this.categories[i].value === this.form.category) {
+          return this.categories[i].label;
+        }
+      }
+      return this.form.category;
+    },
+    getCountry() {
+      for (let i = 0; i < this.countries.length; i += 1) {
+        if (this.countries[i].value === this.form.country) {
+          return this.countries[i].label;
+        }
+      }
+      return this.form.country;
+    },
+    getDays() {
+      for (let i = 0; i < this.DaysOfWeek.length; i += 1) {
+        if (this.DaysOfWeek[i].value === this.form.days) {
+          return this.DaysOfWeek[i].label;
+        }
+      }
+      return this.form.days;
     },
   },
 };
