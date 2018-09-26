dfbf0f01bda249775b55ea30ee2d0a36cc037a30
Eliger Roa <eliger13@gmail.com>
2018-09-26 02:33:51 +0000

    Añadir pantalla de configuracion

---
+++ src/pages/settings.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page padding>
+    <h4>{{ title }}</h4>
+
+  <div class="group ">
+  </div>
+
+  </q-page>
+</template>
+
+<script>
+import { required, minLength, sameAs } from 'vuelidate/lib/validators';
+
+export default {
+  data() {
+    return {
+      title: 'Setting',
+    };
+  },
+
+  validations: {
+    form: {
+      password: { required, minLength: minLength(8) },
+      confirmPassword: { sameAsPassword: sameAs('password') },
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+
+      if (this.$v.form.$error) {
+        this.$q.notify('Please review fields again.');
+      } else {
+        this.$q.notify({
+          message: 'Login successfully.',
+          type: 'positive',
+        });
+
+        this.$router.push('/add');
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
